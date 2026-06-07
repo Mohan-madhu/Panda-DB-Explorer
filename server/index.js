00f2130ec709c1ecd4aa6e23c1e619e0f099b4df
MOHAN <mohan326856@gmail.com>
2026-06-07 19:55:39 +0530

    dotenv added in code

--- server/index.js
+++ server/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const fs = require('fs');
-
+const dotenv = require('dotenv');
+dotenv.config();
 const app = express();
 
 app.use(cors());
